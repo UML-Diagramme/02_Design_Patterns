0dfecaf97a478b6fc89cee80e6b106d3c044e1d4
S-H1990 <Sarahakimi206@yahoo.com>
2024-10-08 09:40:16 +0000

    05/kummulierender Counter

--- com/cc/java/Singleton.java
+++ com/cc/java/Singleton.java
@@ -3,23 +3,37 @@ package com.cc.java;
 
 
 public class Singleton {
-    // objektbildung
+    // objektbildung in der Klasse
 private static Singleton instance;
+
+// feld, auf das nach der objektbildung zugriffen wird
+private int counter;
+
 
 private Singleton() {
    
 }
-
+// in der  Klasse
 public static Singleton getInstance() {
 //     if (instance == null) {
 //        return instance = new Singleton();
 //     }
 //    return instance; 
 
+// oder mit ternären operator
 return instance == null ? instance = new Singleton() : instance;
  
  }
-  
+//   funktionalität im Objekt
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void actOnEvent() {
+        this.counter ++;
+    }
+
 }
 
 
